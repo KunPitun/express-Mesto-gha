eac13f08a1a9c89b329c07b69730f8e1d1044a4b
Nik Nikolaenko <nickynick29a@gmail.com>
2022-10-06 21:28:39 +0300

    PW 14 v 3.1

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
