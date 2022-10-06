cb88bdc753f5a767b07c921200bf65fe40deab1e
Nik Nikolaenko <nickynick29a@gmail.com>
2022-10-06 21:22:50 +0300

    PW 14 v 3

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(helmet());
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -32,7 +34,6 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
