c56b1c0c307421598646e937bc8de19ad12de6e3
Nik Nikolaenko <nickynick29a@gmail.com>
2022-10-07 19:54:03 +0300

    PW 14 v 3.3

--- controllers/users.js
+++ controllers/users.js
@@ -11,21 +11,6 @@ const badRequestErrorMessage = 'Некорректный _id';
 const notFoundErrorMessage = 'Пользователь с данным _id не найден';
 const internalServerErrorMessage = 'Ошибка на стороне сервера';
 const conflictErrorMessage = 'Пользователь с данным email уже зарегистрирован';
-const authMessage = 'Успешная авторизация';
-
-function addTokenCookieToResponse(res, user) {
-  const token = jwt.sign(
-    { _id: user._id },
-    NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-    {
-      expiresIn: '7d',
-    },
-  );
-  res.cookie('jwt', token, {
-    maxAge: 3600000 * 24 * 7,
-    httpOnly: true,
-  });
-}
 
 module.exports.getAllUsers = (req, res, next) => {
   User.find({})
@@ -82,7 +67,6 @@ module.exports.createUser = (req, res, next) => {
       email, password: hash, name, about, avatar,
     }))
     .then((user) => {
-      addTokenCookieToResponse(res, user);
       res.send({
         data: {
           name: user.name,
@@ -162,8 +146,14 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      addTokenCookieToResponse(res, user);
-      res.send({ message: authMessage });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        {
+          expiresIn: '7d',
+        },
+      );
+      res.send({ token });
     })
     .catch((err) => {
       if (err.name === 'UnauthorizedError') {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,11 +5,12 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 const unauthorizedErrorMessage = 'Необходима авторизация';
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new UnauthorizedError(unauthorizedErrorMessage));
     return;
   }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
